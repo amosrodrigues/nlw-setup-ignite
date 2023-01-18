713abfe53b8596d93502391789f6df28e08b16bf
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-01-18 16:50:57 -0300

    web/feat: implement range of the days.

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts';
 import { HabitDay } from './HabitDay';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDateSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDateSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -18,7 +24,19 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
